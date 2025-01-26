bc978dc94a32183216105804587920aa6b1b6560
Diego Neira <diegoneira@gmail.com>
2025-01-26 19:35:28 -0300

    CH2 Nodes L5 LeafNode

--- prueba.py
+++ prueba.py
@@ -1,5 +1,22 @@
-diccionario = { "href":"http", "href2":"http2"}
-texto = ""
+diccionario:dict = { "href":"http", "href2":"http2"}
+texto:str = ""
 for elemento in diccionario:
     texto += " " + elemento + " " + diccionario[elemento]
 print(texto)
+
+variable: int = "hola"
+
+
+class Clase:
+    def __init__(self, arg1:int):
+        self.arg1 = arg1
+
+    def doble_arg1(self):
+        resp = self.arg1 * 2
+        return resp
+
+    def __repr__(self):
+        return f"el doble del argumento es {resp}"
+
+prueba1 = Clase(2)
+print(prueba1)
--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,9 +21,24 @@ class HTMLNode:
 
     def __eq__(self, other):
         return (
-            self.tag == other.tag 
-            and self.value == other.value 
+            self.tag == other.tag
+            and self.value == other.value
             and self.children == other.children
             and self.props == other.props
         )
 
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value riquired")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
